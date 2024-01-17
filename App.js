cba46de56ba32a7f340672d8f9a5b3e0594a42ad
Vito <lolozol80xd@gmail.com>
2024-01-17 12:12:01 +0100

    asd

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, Image, TextInput, Button,  } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.h1}>bakxtale taves</Text>
+      <Image style={styles.kep} source={require('./assets/jq2ohpsp45z91.jpg')} />
+      <Text style={styles.h2}>aves romale</Text>
+      <Text style={styles.h2}>Kik hívnak már ?</Text>
+      <TextInput 
+        style={styles.input} 
+        onChangeText={text => onChangeText(text)}
+        />
+      <Text>{text}</Text>
+      <Button title='Start' onPress={()=>checkAnswer()}/>
     </View>
   );
+  function checkAnswer(){
+    if (text=="öcskösök" || text=="Öcskösök") {
+            
+      alert("Helyes")
+    }
+    alert("Rossz válasz")
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#500197',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  h1: {
+    color: '#fff',
+    fontSize: 44,
+  },
+  h2:{
+    color:"#fff",
+  },
+  kep:{
+    width:500,
+    height:250,
+  },
+  input: {    
+    borderColor: 'blue',
+    borderWidth: 1,
+    backgroundColor: 'lightblue',
+    width: '80%',
   },
 });
